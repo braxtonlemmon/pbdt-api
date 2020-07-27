63fd1463df40b0848055086e1f5de2a46ba3c651
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-07-27 14:20:39 +0200

    Update send-mail.

--- api/send-mail.js
+++ api/send-mail.js
@@ -1,3 +1,29 @@
+const nodemailer = require('nodemailer');
+require("path");
+require("dotenv").config();
+
+const mailer = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
+
 module.exports = (req, res) => {
-  res.status(200).send('heyy');
+    mailer.sendMail(
+      {
+        from: req.body.from,
+        to: process.env.GMAIL_ADDRESS,
+        subject: req.body.subject || "No subject",
+        text: req.body.message || "No message",
+      },
+      function (err, info) {
+        if (err) return res.status(500).send(err);
+        res.json({ success: true });
+      }
+    );
+
 }
